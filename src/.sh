0132534278cbb5acb53502ef1557131b866f39d2
planetrenox <planetrenox@protonmail.com>
2022-11-10 17:10:35 -0800

    include rustup/cargo with project

--- src/.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# curl https://sh.rustup.rs -sSf | sh -s -- --profile complete --default-toolchain nightly
-
-cargo clean \
-&& cargo build --target wasm32-unknown-unknown \
-&& cp ../target/wasm32-unknown-unknown/debug/enscry.wasm .wasm \
-&& cargo clean \
-
