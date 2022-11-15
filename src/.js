8361eae561e1a49d5df9ff1c483a60d29cb80433
planetrenox <planetrenox@protonmail.com>
2022-11-14 21:52:46 -0800

    speck progress

--- src/.js
+++ src/.js
@@ -1,4 +1,4 @@
-const createSpeck = require('generic-speck')
+const createSpeck = require('generic-speck') // https://www.npmjs.com/package/generic-speck
 
 const speck48_96 = createSpeck({
     bits: 24,
@@ -6,10 +6,17 @@ const speck48_96 = createSpeck({
     rightRotations: 8,
     leftRotations: 3
 })
-// const key = [0x020100, 0x0a0908, 0x121110, 0x1a1918]
-// const originalInteger = 0x6968746d2073
-// const obfuscatedInteger = speck48_96.encrypt(originalInteger, key)
-// console.log(obfuscatedInteger.toString(16)) 
+
+// https://eprint.iacr.org/2013/404.pdf#page=17
+// Speck 48/96
+// Key: 1a1918 121110 0a0908 020100
+// Plaintext: 6d2073 696874
+// Ciphertext: 735e10 b6445d
+
+const key = [0x020100, 0x0a0908, 0x121110, 0x1a1918]
+const originalInteger = 0x6968746d2073
+const obfuscatedInteger = speck48_96.encrypt(originalInteger, key)
+console.log(obfuscatedInteger.toString(16))
 
 
 const textarea = document.getElementById("textarea")
@@ -41,11 +48,18 @@ function ACT (event)
 /** Zero Width Unicode Standard — Senary */
 const DES = {
     code: {
-        0: "\u{180E}", 1: "\u{200B}", 2: "\u{200C}", 3: "\u{200D}", 4: "\u{200E}", 5: "\u{2060}", unifier: "\u{FEFF}"
+        0: "\u{180E}",
+        1: "\u{200B}",
+        2: "\u{200C}",
+        3: "\u{200D}",
+        4: "\u{200E}",
+        5: "\u{2060}",
+        unifier: "\u{FEFF}"
     },
 
     encode: (text) => Array.from(text).map(x => x.codePointAt(0).toString(6).split('').map(x => DES.code[x]).join('')).join(DES.code.unifier),
     decode: (text) => text.split(DES.code.unifier).map(x => String.fromCodePoint(parseInt(Array.from(x).map(z => Object.keys(DES.code).find(k => DES.code[k] === z)).join(''), 6))).join(''),
+    
     CRY: {
         NO: {
             SPECK48_96: () => "worDks",
@@ -57,4 +71,4 @@ const DES = {
     }
 };
 
-// https://soundcloud.com/crystal-castles/pino-placentile-wooden-girl
+// https://soundcloud.com/esudesu/tried-luvletter
