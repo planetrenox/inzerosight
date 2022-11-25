78ad7483a5b6eeedde7b4a3d7bad72ad8fab0be0
planetrenox <planetrenox@protonmail.com>
2022-11-24 20:17:13 -0800

    COMMENT

--- src/.js
+++ src/.js
@@ -50,6 +50,10 @@ const ZWUS6 = {
             NO: {
                 SPECK48_96: (pt24, kStr) =>
                 {
+                    // TODO use stringbuilder for pt and feed it to the speck function
+                    
+                    
+                    
                     const key96 = blake.blake2bHex(kStr, null , 12) // expand key to fixed length of 96 bits
                     let key96arr = [parseInt(key96.slice(0, 6), 16), parseInt(key96.slice(6, 12), 16), parseInt(key96.slice(12, 18), 16), parseInt(key96.slice(18, 24), 16)]
                     const obfuscatedInteger = speck.encrypt(pt24, key96arr)
